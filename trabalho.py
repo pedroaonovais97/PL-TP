eaad94d0a02ba394f9a1289a1ee344f8d43a078e
Pedro Novais <49559911+thexx782@users.noreply.github.com>
2021-03-27 23:02:36 +0000

    Add files via upload

--- trabalho.py
+++ trabalho.py
@@ -1,8 +1,9 @@
 import re
 
 f = open('processos.xml')
-
+next(f)
 anosList = []
+processos = []
 anos = {}
 seculos = {}
 pnome = {}
@@ -17,91 +18,190 @@ seculo18Ultimo = {}
 seculo19Ultimo = {}
 seculo20Ultimo = {}
 
+global ano
+global sec
+global anoList
+global primeironome
+global ultimonome
+global completo
+global pr
+
+
+contador = 1
+
 for line in f:
 
 	m = re.search(r'(<obs/>)|(<processo id="([0-9]+)">)|(<[a-z]+>)?([^<]*)(</[a-z]+>)?',line.strip())
+	if m.group(4) == '<processos>' or m.group(6) == '</processos>':
+		pass
+	if "processo id" in str(m.group(2)):
+		pr = m.group(3)
+		if pr in processos:
+			next(f)
+			next(f)
+			next(f)
+			next(f)
+			next(f)
+			next(f)
+			next(f)
+		else:
+			processos.append(pr)
+	else:
+		print(contador,'->',m.groups())
+		contador += 1	
+		for g in m.groups():
+			if g is None:
+				pass	
+			elif g == '<data>':
 
-	for g in m.groups():
-		if g is None:
-			pass
-		elif g == '<data>':
+				ano = re.split(r'-',m.group(5))[0]
+				sec = int(ano[0]+(ano[1])) + 1
+				anoList = int(ano)
 
-			ano = re.split(r'-',m.group(5))[0]
-			sec = int(ano[0]+(ano[1])) + 1
-			anoList = int(ano)
-
-			if anoList in anosList:
-				pass
-			else:
-				anosList.append(anoList)	
-
-			if ano in anos:
-				anos[ano] += 1
-			else:
-				anos[ano] = 1
-
-			if sec in seculos:
-				seculos[sec] += 1
-			else:
-				seculos[sec] = 1				
-
-		elif g == '<nome>':
-			primeironome = 	re.split(r' ',m.group(5))[0]
-			ultimonome = re.split(r' ',m.group(5))[-1]
-			completo = m.group(5)	
-
-			if primeironome in pnome:
-				pnome[primeironome] += 1
-			else:
-				pnome[primeironome] = 1
-
-			if ultimonome in unome:
-				unome[ultimonome] += 1
-			else:
-				unome[ultimonome] = 1
-
-			if sec == 17 :
-				if primeironome in seculo17:
-					seculo17[primeironome] += 1
+				if anoList in anosList:
+					pass
 				else:
-					seculo17[primeironome] = 1
+					anosList.append(anoList)	
 
-				if ultimonome in seculo17Ultimo:
-					seculo17Ultimo[ultimonome] += 1
+				if ano in anos:
+					anos[ano] += 1
 				else:
-					seculo17Ultimo[ultimonome] = 1		
-			if sec == 18 :
-				if primeironome in seculo18:
-					seculo18[primeironome] += 1
-				else:
-					seculo18[primeironome] = 1
+					anos[ano] = 1
 
-				if ultimonome in seculo18Ultimo:
-					seculo18Ultimo[ultimonome] += 1
+				if sec in seculos:
+					seculos[sec] += 1
 				else:
-					seculo18Ultimo[ultimonome] = 1	
-			if sec == 19 :
-				if primeironome in seculo19:
-					seculo19[primeironome] += 1
-				else:
-					seculo19[primeironome] = 1
+					seculos[sec] = 1				
 
-				if ultimonome in seculo19Ultimo:
-					seculo19Ultimo[ultimonome] += 1
+			elif g == '<nome>':
+				primeironome = 	re.split(r' ',m.group(5))[0]
+				ultimonome = re.split(r' ',m.group(5))[-1]
+				completo = m.group(5)	
+				
+				if primeironome in pnome:
+					pnome[primeironome] += 1
 				else:
-					seculo19Ultimo[ultimonome] = 1	
-			if sec == 20 :
-				if primeironome in seculo20:
-					seculo20[primeironome] += 1
+					pnome[primeironome] = 1
+
+				if ultimonome in unome:
+					unome[ultimonome] += 1
 				else:
-					seculo20[primeironome] = 1
+					unome[ultimonome] = 1
 
-				if ultimonome in seculo20Ultimo:
-					seculo20Ultimo[ultimonome] += 1
+				if sec == 17 :
+					if primeironome in seculo17:
+						seculo17[primeironome] += 1
+					else:
+						seculo17[primeironome] = 1
+
+					if ultimonome in seculo17Ultimo:
+						seculo17Ultimo[ultimonome] += 1
+					else:
+						seculo17Ultimo[ultimonome] = 1		
+				if sec == 18 :
+					if primeironome in seculo18:
+						seculo18[primeironome] += 1
+					else:
+						seculo18[primeironome] = 1
+
+					if ultimonome in seculo18Ultimo:
+						seculo18Ultimo[ultimonome] += 1
+					else:
+						seculo18Ultimo[ultimonome] = 1	
+				if sec == 19 :
+					if primeironome in seculo19:
+						seculo19[primeironome] += 1
+					else:
+						seculo19[primeironome] = 1
+
+					if ultimonome in seculo19Ultimo:
+						seculo19Ultimo[ultimonome] += 1
+					else:
+						seculo19Ultimo[ultimonome] = 1	
+				if sec == 20 :
+					if primeironome in seculo20:
+						seculo20[primeironome] += 1
+					else:
+						seculo20[primeironome] = 1
+
+					if ultimonome in seculo20Ultimo:
+						seculo20Ultimo[ultimonome] += 1
+					else:
+						seculo20Ultimo[ultimonome] = 1
+			elif g == '<obs>':
+				scdsearch = re.split(r'\.',m.group(5))
+				scddoc = re.search(r'Doc\.danificado',m.group(5))
+				familia = re.findall(r'.+,Ti. .aterno|.+,Irma.*|.+,Prim.|.+,Sobrinh. .atern.',m.group(5))
+				#print('PESQUISA: ',scdsearch)
+				'''
+				if scddoc:
+					print('DOC DAN!!!')
+					
+					if anos[ano] > 1:
+						anos[ano] -= 1
+					else:
+						del anos[ano]
+					
+					if seculos[sec] > 1:
+						seculos[sec] -= 1
+					else:
+						del seculos[sec]
+
+					if pnome[primeironome] > 1:
+						pnome[primeironome] -= 1
+					else:
+						del pnome[primeironome]
+
+					if unome[ultimonome] > 1:
+						unome[ultimonome] -= 1
+					else:
+						unome[ultimonome] = 1
+
+					if sec == 17 :
+						if seculo17[primeironome] > 1:
+							seculo17[primeironome] -= 1
+						else:
+							del seculo17[primeironome]
+
+						if seculo17Ultimo[ultimonome] > 1:
+							seculo17Ultimo[ultimonome] -= 1
+						else:
+							del seculo17Ultimo[ultimonome]		
+					if sec == 18 :
+						if seculo18[primeironome] > 1:
+							seculo18[primeironome] -= 1
+						else:
+							del seculo18[primeironome]
+
+						if seculo18Ultimo[ultimonome] > 1:
+							seculo18Ultimo[ultimonome] -= 1
+						else:
+							del seculo18Ultimo[ultimonome]	
+					if sec == 19 :
+						if seculo19[primeironome] > 1:
+							seculo19[primeironome] -= 1
+						else:
+							del seculo19[primeironome]
+
+						if seculo19Ultimo[ultimonome] > 1:
+							seculo19Ultimo[ultimonome] -= 1
+						else:
+							del seculo19Ultimo[ultimonome]		
+					if sec == 20 :
+						if seculo20[primeironome] > 1:
+							seculo20[primeironome] -= 1
+						else:
+							del seculo20[primeironome]
+
+						if seculo20Ultimo[ultimonome] > 1:
+							seculo20Ultimo[ultimonome] -= 1
+						else:
+							del seculo20Ultimo[ultimonome]
 				else:
-					seculo20Ultimo[ultimonome] = 1									
-
-
+					print('FAMILIA: ',familia)
+				'''				
+							
+						
 sorted_pnome = dict(sorted(pnome.items(), key=lambda p:p[1],reverse = True))
 sorted_unome = dict(sorted(unome.items(), key=lambda p:p[1],reverse = True))
 sorted_anos = dict(sorted(anos.items(), key=lambda p:p[0]))
@@ -115,6 +215,8 @@ sorted_unome_secolo18 = dict(sorted(seculo18Ultimo.items(), key=lambda p:p[1],re
 sorted_unome_secolo19 = dict(sorted(seculo19Ultimo.items(), key=lambda p:p[1],reverse = True))
 sorted_unome_secolo20 = dict(sorted(seculo20Ultimo.items(), key=lambda p:p[1],reverse = True))
 anosList.sort()
+
+print(processos)
 
 print('INTERVALOS SEM INSCRICOES:\n')
 i = 0
